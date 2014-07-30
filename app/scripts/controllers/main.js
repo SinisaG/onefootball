6c714bc9a04de8a8236059fde34cc3d1dca938ce
Sinisa <sinisa@hackandcraft.com>
2014-07-30 22:08:01 +0200

    layout issues in ie fixed, players sorted by number

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,14 @@
 var mainCtrl = angular.module('onefootballApp');
     mainCtrl.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
         $http.get('/teams/bayern.json').success(function(data) {
-            $scope.team = data;
-        });
+            data.players = data.players.sort(compare);
+			$scope.team = data;
+		});
+		function compare(a,b) {
+		  if (a.number < b.number)
+			 return -1;
+		  if (a.number > b.number)
+			return 1;
+		  return 0;
+		};
     }]);
